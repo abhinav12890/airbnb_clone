9e7f2da6f6356a98244ebc28b89bb2882c881caa
Abhinav Kumar <abhinavkumarpal890@gmail.com>
2023-09-01 19:46:30 +0530

    Resolved conflicts for security

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -274,7 +274,7 @@ const RentModal = () => {
     <Modal
       disabled={isLoading}
       isOpen={rentModal.isOpen}
-      title="Airbnb your home!"
+      title="Getflat your home!"
       actionLabel={actionLabel}
       onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
